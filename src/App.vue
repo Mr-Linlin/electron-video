c8ab2497ce77a8e05a2a580520c3596dd588688e
‘linzhentao’ <1908878835@qq.com>
2023-10-12 15:35:15 +0800

    #1

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main">
+    <button class="btn" @click="handleChangeTheme">切换主题</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const bol = ref<Boolean>(false);
+const handleChangeTheme = () => {
+  bol.value = !bol.value;
+  if (bol.value) {
+    document.body.className = 'dark';
+  } else {
+    document.body.className = '';
+  }
+};
+</script>
+<style lang="scss" scoped>
+.main {
+  width: 100vw;
+  height: 100vh;
+  color: red;
+  background: var(--theme-bg-color);
+  .btn {
+    color: var(--btn-color);
+  }
+}
+</style>
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,49 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import VuePlugin from "@vitejs/plugin-vue";
+import path from 'path'
+const resolve = (dir: string) => path.join(__dirname, dir)
+
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  base: './',
+  plugins: [VuePlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      components: resolve('src/components'),
+      api: resolve('src/api'),
+      views: resolve('src/views'),
+      utils: resolve('src/utils'),
+      routes: resolve('src/routes'),
+      styles: resolve('src/styles'),
+      model: path.resolve(__dirname, 'src/model'),
+      store: path.resolve(__dirname, 'src/store'),
+    },
+    extensions: [".js", ".ts", ".tsx", ".jsx"],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`,
+      },
+      less: {
+        math: 'parens-division',
+      },
+    },
+    modules: {
+      localsConvention: 'camelCase'
+    }
+
+  },
+  server: {
+    proxy: {
+      // 选项写法
+      '/api': {
+        target: 'http://127.0.0.1:7003/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
